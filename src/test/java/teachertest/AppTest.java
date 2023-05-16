145fc670c1d135ae40f61d9c9843b21366aef7a2
admin <hang.nguyen@restaff.no>
2023-05-16 17:01:36 +0700

    [Modified]: Apply Method Chaining in Code

--- src/test/java/teachertest/AppTest.java
+++ src/test/java/teachertest/AppTest.java
@@ -6,24 +6,29 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import teachertest.drive.Driver;
-import teachertest.action.TeacherAction;
+import teachertest.step.TeacherTestSteps;
 
 
 public class AppTest extends TestCase {
 
     private WebDriver webDriver;
     private Driver driver = new Driver(webDriver);
-    private TeacherAction teacherService;
+    private TeacherTestSteps testSteps;
 
     @Before
     public void setUp() {
         driver.setUp();
-        teacherService = new TeacherAction(driver.getWebDriver());
+        testSteps = new TeacherTestSteps(driver.getWebDriver());
     }
 
     @Test
-    public void testAddTeacher() throws InterruptedException {
-        teacherService.addTeacher();
+    public void testAddTeacher() {
+        testSteps.addTeacher();
+    }
+
+    @Test
+    public void testSearchTeacher() {
+        testSteps.searchTeacher();
     }
 
     @After
