fc8e84345d698301e26e96a5e1631cd5a8d78e54
cuongnguyen-oceansmart <cuongnguyen3900@gmail.com>
2023-05-16 15:26:23 +0700

    [Modified] StudentAddTest: create new Find.java,make use of Builder to build an object for chaining methods.

--- src/test/java/student/student_add_test/StudentAddTest.java
+++ src/test/java/student/student_add_test/StudentAddTest.java
@@ -1,28 +1,33 @@
 package student.student_add_test;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import student.student_add_test.actions.Enter;
+import student.student_add_test.actions.Find;
 import student.student_add_test.page.CreateStudentPage;
 import student.student_add_test.page.StudentPage;
 import student.student_add_test.page.StudentUI;
 import student.student_add_test.webdriver.ChromeDriverSetup;
 import student.student_add_test.webdriver.WebDriverSetup;
-import student.student_search_test.student_entity.Student;
+import student.student_add_test.entity.Student;
 
-import java.util.ArrayList;
-
+@Getter
+@Setter
 public class StudentAddTest {
     private WebDriverSetup webDriverSetup;
     private WebDriver driver;
     private StudentPage studentPage;
     private CreateStudentPage createStudentPage;
     private Student student;
+    private Enter enter;
 
     @Before
     public void setUp() {
-        student = new Student("Jack", "Jill", "Billy@gill.com");
+        student = new Student("Builder", "method", "build=me@gmail.com");
         webDriverSetup = new ChromeDriverSetup();
         driver = webDriverSetup.getDriver();
         studentPage = new StudentPage(driver);
@@ -31,8 +36,15 @@ public class StudentAddTest {
     @Test
     public void testAddStudent() {
         studentPage.navigateStudent(StudentUI.STUDENT_PAGE_URL);
-        studentPage.clickAddStudent(StudentUI.ADD_STUDENT_BUTTON);
-        createStudentPage.fillFirstName(StudentUI.INPUT_ID_FIRST_NAME, student.getFirstName());
+
+
+        Find.element(StudentUI.ADD_STUDENT_BUTTON).click();
+        Enter.theValue(student.getFirstName()).into(StudentUI.INPUT_ID_FIRST_NAME);
+        /*Click.On("");
+        Tap.On("");
+        Select.option("car").into("serlectobox");
+        Drag.element("d").to("dd");*/
+
         createStudentPage.fillLastName(StudentUI.INPUT_ID_LAST_NAME, student.getLastName());
         createStudentPage.fillEmail(StudentUI.INPUT_ID_EMAIL, student.getEmail());
         createStudentPage.clickSubmit(StudentUI.SUBMIT_BUTTON);
---
+++ src/test/java/student/student_add_test/actions/Find.java
@@ -0,0 +1,21 @@
+package student.student_add_test.actions;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import student.student_add_test.page.BaseActions;
+
+public class Find extends BaseActions {
+    public Find(WebDriver driver) { super(driver); }
+
+    public static FindBuilder element(String addStudentButton) {
+        return new FindBuilder(addStudentButton);
+    }
+    public static class FindBuilder {
+        private String addStudentButton;
+        public FindBuilder(String addStudentButton) { this.addStudentButton = addStudentButton; }
+
+        public void click() {
+            findElementByXpath(addStudentButton).click();
+        }
+    }
+}
